ffda960c95135d4e5ecef353517d63f542853563
Tyson Williams <tyson@Tysons-MacBook-Pro.local>
2015-01-13 13:12:00 -0800

    added flickrSearch class

---
+++ FlickrSearch/FlickrPhotosViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrPhotosViewController.swift
+//  FlickrSearch
+//
+//  Created by Tyson Williams on 1/12/15.
+//  Copyright (c) 2015 Tyson Williams. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotosViewController: UICollectionViewController {
+    private let reuseIdentifier = "flickrCell"
+}
